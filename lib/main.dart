5c912617448a33853054e1acf4796293a9991395
Kelven <kelvengalvao@gmail.com>
2019-04-12 17:43:05 -0300

    Project started and firebase configurated.

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  Firestore.instance.collection('teste').document('teste').setData({'teste': 'teste'});
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
